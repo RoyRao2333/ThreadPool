de36943b79b6a43c40db8d252ca63caa12a18eb0
Roy <royrao2333@gmail.com>
2020-12-02 18:39:28 +0800

    Add files via upload

---
+++ ThreadPool/main.cpp
@@ -0,0 +1,48 @@
+//
+//  main.cpp
+//  ThreadPool
+//
+//  Created by Roy Rao on 2020/12/2.
+//
+
+#include <string>
+#include "threadpool.hpp"
+
+void foo_1() {
+    cout << "Executing first foo." << endl;
+}
+
+int foo_2() {
+    cout << "Executing second foo." << endl;
+    return 666;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    try {
+        ThreadPool pool { 4 };
+        future<void> f1 = pool.commit(foo_1);
+        future<int> f2 = pool.commit(foo_2);
+        future<string> f3 = pool.commit( []() -> string {
+            cout << "Executing third foo." << endl;
+            return "third foo";
+        });
+        
+        pool.stop();
+        
+        f1.get();
+        cout << "I can access their return value here: " << f2.get() << "\t" << f3.get() << endl;
+        this_thread::sleep_for(chrono::seconds(5));
+        
+        pool.resume();
+        
+        // different way of writting
+        pool.commit(foo_1).get();
+        
+        cout << "finished!" << endl;
+        
+        return 0;
+    } catch (exception &e) {
+        cout << "something happened: " << e.what() << endl;
+    }
+}
---
+++ ThreadPool/threadpool.hpp
@@ -0,0 +1,140 @@
+//
+//  threadpool.hpp
+//  ThreadPool
+//
+//  Created by Roy Rao on 2020/12/2.
+//
+
+#ifndef threadpool_hpp
+#define threadpool_hpp
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <atomic>
+#include <stdexcept>
+using namespace std;
+using Task = function<void()>;
+
+#endif /* threadpool_hpp */
+
+class ThreadPool {
+// MARK: - init & deinit
+public:
+    ThreadPool(size_t);
+    ~ThreadPool();
+    
+private:
+    // forbid copying
+    ThreadPool(const ThreadPool&);
+    
+// MARK: - foos
+public:
+    template<typename F, typename... Args>
+    auto commit(F&& f, Args&&... args) -> future<decltype(f(args...))>;
+    void stop();
+    void resume();
+    size_t available() { return this->spareThread; }
+    
+private:
+    Task fetchTask();
+    void schedual();
+    
+//  MARK: - attributes
+private:
+    vector<thread> pool;
+    queue<Task> tasks;
+    mutex mtx;
+    condition_variable cv;
+    atomic<bool> isStopped;
+    atomic<size_t> spareThread;
+};
+
+
+
+/*! @brief Initialize a pool with given size.. */
+inline ThreadPool::ThreadPool(size_t poolSize): isStopped(false) {
+    this->spareThread = poolSize < 1 ? 1 : poolSize;
+    for (size_t i = 0; i < this->spareThread; i++) {
+        this->pool.emplace_back( [this] {
+            while (true) {
+                Task task;
+                // fetch one pending task
+                {
+                    unique_lock<mutex> ulock(this->mtx);
+                    // wait for next available task
+                    this->cv.wait(ulock, [this] {
+                        return this->isStopped || !this->tasks.empty();
+                    });
+                    if (this->isStopped && this->tasks.empty()) {
+                        return;
+                    }
+                    // fetch
+                    task = move(this->tasks.front());
+                    this->tasks.pop();
+                }
+                this->spareThread--;
+                task();
+                this->spareThread++;
+            }
+        });
+    }
+}
+
+/*! @brief Wait for all threads to finish thier tasks and destruct. */
+inline ThreadPool::~ThreadPool() {
+    {
+        // lock this block
+        unique_lock<mutex> ulock(this->mtx);
+        this->isStopped = true;
+    }
+    // wake all threads to finish their tasks
+    cv.notify_all();
+    for(thread &thd: pool) {
+        if (thd.joinable()) {
+            // wait for it to finish (only if it can be finished)
+            thd.join();
+        } else {
+            // let it be: thd.detach();
+        }
+    }
+}
+
+/*! @brief Commit a task to current queue and ramdomly wake one. */
+template<typename F, typename... Args>
+auto ThreadPool::commit(F&& foo, Args&&... args) -> future<decltype(foo(args...))> {
+    using ReturnType = decltype(foo(args...));
+    auto task = make_shared<packaged_task<ReturnType()>>(
+        bind(forward<F>(foo), forward<Args>(args)...)
+    );
+    future<ReturnType> result = task->get_future();
+    // emplace task to queue
+    {
+        // lock this block
+        unique_lock<mutex> ulock(this->mtx);
+        if (this->isStopped) {
+            throw runtime_error("ThreadPool have stopped committing.");
+        }
+        this->tasks.emplace( [task]() {
+            (*task)();
+        });
+    }
+    // wake one to execute
+    cv.notify_one();
+    return result;
+}
+
+/*! @brief Stop accepting tasks. */
+void ThreadPool::stop() {
+    this->isStopped.store(true);
+}
+
+/*! @brief Start accepting tasks. */
+void ThreadPool::resume() {
+    this->isStopped.store(false);
+}
